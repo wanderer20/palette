626a6afb092ee35d64e4ae306ce10bfd9fe8a082
Oleg <alizon@bk.ru>
2023-07-23 00:04:21 +0300

    Изменен путь для страниц

---
+++ docs/assets/default-70ddb097.js
@@ -0,0 +1,1 @@
+import{_ as r,r as t}from"./index-b16b43a6.js";const s={};function n(e,o){return t(e.$slots,"default")}const c=r(s,[["render",n]]);export{c as default};
--- docs/assets/default-d10a2976.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,r as t}from"./index-39b2888f.js";const s={};function n(e,o){return t(e.$slots,"default")}const c=r(s,[["render",n]]);export{c as default};
